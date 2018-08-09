3c9f15d03ac41386858ac3762335327884efc0ce
SIERA <siera@SIERAs-Air.localdomain>
2018-08-09 12:31:13 +0700

    fix undefined

--- exercise-2.js
+++ exercise-2.js
@@ -1,7 +1,7 @@
 var word = "hello world!"
 var getLength = word.length
 var print = ''
-for(i=getLength; i>=0; i--){
+for(i=getLength-1; i>=0; i--){
     print = print + word[i]
 }
 console.log(print)
