b53c1280b786becbb9cba41ce610c55ee7295bb2
SIERA <siera@SIERAs-Air.localdomain>
2018-08-09 12:36:39 +0700

    fix description

--- exercise-3.js
+++ exercise-3.js
@@ -12,17 +12,17 @@ var input = [
     ["0003", "Winona", "Ambon", "25/12/1965", "Memasak"],
     ["0004", "Bintang Senjaya", "Martapura", "6/4/1970", "Berkebun"]
 ]
-console.log(input[2])
 
-function dataHandling(noUrut){
+function dataHandling(){
+limit = input.length //4
+for(i=0; i<limit; i++){
     
-    var tmp = input[noUrut]
-    var limit = tmp.length
-    
-    for(i=0; i<limit; i++ ){
-        console.log(tmp[i])
-    }
-
-
+console.log('Nomor ID:',input[i][0]) //0,0 - 1,0 - 2,0 - 3,0 - 4,0
+console.log('Nama Lengkap: ',input[i][1]) 
+console.log('TTL:  ',input[i][2] + ' ' +input[i][3]) 
+console.log('Hobi: ',input[i][4])
+console.log('\n')
 }
-dataHandling(3)
+
+} 
+dataHandling()
